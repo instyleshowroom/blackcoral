de45cb321cb3037c66e0641fd9ad2edd70b8f1e3
timoanzalone <timo.anzalone@gmail.com>
2019-11-27 22:14:27 +0100

    Update nav

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -37,12 +37,10 @@ $('.navbar-item.ss19').hide();
     function () {
       $('.navbar-item.ss19').fadeIn(300);
       $('.navbar-item.fw19').fadeIn(300);
-      $('.collections-link').css('height','84px');
     },
     function () {
       $('.navbar-item.ss19').fadeOut(300);
       $('.navbar-item.fw19').fadeOut(300);
-      $('.collections-link').css('height','auto'), 300;
     }
   );
 
